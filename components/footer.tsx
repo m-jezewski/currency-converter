0f19b1f9aebea8b56444d5afccfe8310e8c1d0e5
Karrius <Ecetris@gmail.com>
2022-10-25 14:12:58 +0200

    page structure

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+interface FooterProps {
+
+}
+
+const Footer = ({ }: FooterProps) => {
+    return (
+        <footer className="bg-emerald-900 text-white p-2 text-left text-lg">
+            <a href='#'>Github</a>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ components/header.tsx
@@ -0,0 +1,13 @@
+interface HeaderProps {
+
+}
+
+const Header = ({ }: HeaderProps) => {
+    return (
+        <header className='bg-emerald-900 text-white p-2 text-center text-2xl'>
+            Currency Converter
+        </header>
+    );
+}
+
+export default Header;
